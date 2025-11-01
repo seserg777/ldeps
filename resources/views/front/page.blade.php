8105b05a4a24f7240a165a87173a6797ecfa3f04
seserg777 <seserg777@gmail.com>
2025-11-01 13:12:21 +0100

    fix: update page.blade.php to properly handle pageData structure
    - Use pageData for dynamic content rendering
    - Support different component types (Content, Banners, etc)
    - Display menu item title as H1
    - Maintain same header/footer structure as homepage
    - Support modules in all positions
    - Fix compatibility with controllers that pass pageData

--- resources/views/front/page.blade.php
+++ resources/views/front/page.blade.php
@@ -1,37 +1,109 @@
 @extends('share.layouts.base')
 
 @php
-    $componentType = $pageData['componentType'] ?? 'default';
-    // Map component types to proper names for CSS classes
-    $componentClass = match($componentType) {
-        'Content' => 'content',
-        'ContentList' => 'content-list', 
-        'Exussalebanner' => 'banner',
-        'ExussalebannerList' => 'banner-list',
-        default => 'default'
-    };
+    $componentType = $pageData['componentType'] ?? 'page';
+    $menuItem = $pageData['menuItem'] ?? null;
 @endphp
 
-@section('title', $pageData['menuItem']['title'] . ' - ' . $pageData['siteName'])
+@section('title', $menuItem['title'] ?? 'Page')
+@section('description', $pageData['siteDescription'] ?? '')
 
 @section('content')
-  <div class="page {{ $componentClass }}">
-    {{-- Top menu SSR --}}
-    <nav class="site-menu menu-top">
-      {!! $menuTopHtml ?? '' !!}
-    </nav>
+  <div class="page page-{{ $componentType }}">
+    <section class="top">
+        <div class="container">
+            {{-- Top menu SSR --}}
+            <nav class="site-menu menu-top menu-main-menu-add">
+                {!! $menuTopHtml ?? '' !!}
+            </nav>
+            
+            {{-- Modules: Top position --}}
+            @include('share.layouts.partials.modules_position', ['position' => 'top'])
+        </div>
+    </section>
+    
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <a href="/" class="logo">
+                        <img src="{{ asset('images/logo.svg') }}" alt="Logo">
+                    </a>
+                </div>
+            
+                <div class="col-7">
+                    {{-- Main menu SSR --}}
+                    <nav class="site-menu menu-main menu-mainmenu-rus">
+                        {!! $menuMainHtml ?? '' !!}
+                    </nav>
+                </div>
 
-    {{-- Main menu SSR --}}
-    <nav class="site-menu menu-main">
-      {!! $menuMainHtml ?? '' !!}
-    </nav>
+                <div class="col-3">
+                    @include('share.layouts.partials.search')
+                </div>
+            </div>
+            
+            {{-- Modules: Header position --}}
+            @include('share.layouts.partials.modules_position', ['position' => 'header'])
+        </div>
+    </header>
 
+    {{-- Page content --}}
     <main>
-      {!! $pageContentHtml ?? '' !!}
+        {{-- Modules: Content-top position --}}
+        <div class="container">
+            @include('share.layouts.partials.modules_position', ['position' => 'content-top'])
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    @if($menuItem)
+                        <h1>{{ $menuItem['title'] }}</h1>
+                    @endif
+                    
+                    {{-- Dynamic content based on component type --}}
+                    @if($componentType === 'Content' || $componentType === 'ContentList')
+                        <div class="content-area">
+                            {!! $pageData['additionalData']['content'] ?? '' !!}
+                        </div>
+                    @elseif($componentType === 'Exussalebanner' || $componentType === 'ExussalebannerList')
+                        <div class="banners-area">
+                            {{-- Banners content --}}
+                        </div>
+                    @else
+                        <div class="page-content">
+                            {!! $pageContentHtml ?? '' !!}
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+        
+        {{-- Modules: Content-bottom position --}}
+        <div class="container">
+            @include('share.layouts.partials.modules_position', ['position' => 'content-bottom'])
+        </div>
     </main>
 
     @isset($footerHtml)
-      <footer>{!! $footerHtml !!}</footer>
+      <footer>
+        {!! $footerHtml !!}
+        
+        {{-- Modules: Footer position --}}
+        <div class="container">
+            @include('share.layouts.partials.modules_position', ['position' => 'footer'])
+        </div>
+      </footer>
     @endisset
+    
+    {{-- Modules: Bottom position --}}
+    <div class="container">
+        @include('share.layouts.partials.modules_position', ['position' => 'bottom'])
+    </div>
   </div>
 @endsection
+
+@push('scripts')
+@endpush
+
